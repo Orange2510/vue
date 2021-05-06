4cef45f7f429af7e321caeddf43a954c9555e412
qiuyun <410220572@qq.com>
2021-05-06 21:09:47 +0800

    完成拦截器

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
   <div>
     <router-view />
     <Footer v-if="!$route.meta.isHide" />
+    <!-- 测试拦截器 -->
+    <!-- <button @click="handleClick">按钮</button> -->
   </div>
 </template>
 
 <script >
+// 测试拦截器
+// import request from './utils/request'
 export default {
   name: "App",
   computed: {
@@ -14,8 +18,23 @@ export default {
     //   return path !== '/login' && path !== '/register'
     // }
   },
-  mounted () {
-
+  methods: {
+    // 测试拦截器
+    // handleClick () {
+    //   request({
+    //     url: 'http://39.98.123.211/api/user/passport/login',
+    //     method: 'POST',
+    //     headers: {
+    //       token: 'XXX',
+    //     },
+    //     data: {
+    //       phone: '13700000000',
+    //       password: '111111',
+    //     }
+    //   }).catch(err => {
+    //     console.log(err);
+    //   })
+    // }
   }
 }
 </script>
