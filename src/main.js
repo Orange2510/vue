4132ed488b195066b0f487fff357055ed8c8096c
qiuyun <410220572@qq.com>
2021-05-06 21:28:30 +0800

    Home静态页面完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import Footer from './components/Footer'
+import TypeNav from './components/TypeNav'
 
 import router from './router'
 import store from './store'
@@ -10,6 +11,7 @@ import './assets/css/normalize.css'
 Vue.config.productionTip = false
 
 Vue.component('Footer', Footer)
+Vue.component('TypeNav', TypeNav)
 
 new Vue({
   router,
